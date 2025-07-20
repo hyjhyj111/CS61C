9eef0ffcf7cb51700cc9c1d220890da7279c28ed
Yujie He <heyujie0711@outlook.com>
2025-07-20 03:09:10 +0000

    lab01

--- lab/lab01/ll_cycle.c
+++ lab/lab01/ll_cycle.c
@@ -3,5 +3,13 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *p = head, *q = head;
+    while (q != NULL && q->next != NULL && q->next->next != NULL) {
+        p = p->next;
+        q = q->next->next;
+        if (p == q && p != NULL) {
+            return 1;
+        }
+    }
     return 0;
 }
